fa2a4b7aa1904ce134b0a1f92d4200e5a2bc553c
slannec <nick104@outlook.com>
2024-11-16 18:25:26 -0300

    criando tela principal do app

--- app/src/main/java/com/itsmenicky/agendadecontatos/MainActivity.kt
+++ app/src/main/java/com/itsmenicky/agendadecontatos/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.itsmenicky.agendadecontatos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.itsmenicky.agendadecontatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCadastrar.setOnClickListener{
+            val tela_cadastro = Intent(this, cadastro_usuario::class.java)
+            startActivity(tela_cadastro)
+        }
     }
 }
